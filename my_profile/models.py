b8aae36d51adf7358ff0c7305941fa3585b210ad
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-10-22 22:03:48 +0000

    Add views/models to profile

--- my_profile/models.py
+++ my_profile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Corrected
+    sending_address = models.CharField(max_length=50, blank=True, null=True)
+    receiving_address = models.CharField(max_length=50, blank=True, null=True)
+    notificationEmail = models.BooleanField(default=True)
+    showLocation = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.user.username
+        
--- my_profile/views.py
+++ my_profile/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def my_profile(request):
-    return render(request, 'profile.html')
+     profile = get_object_or_404(Profile, user=request.user)
+     return render(request, 'profile.html', {
+        'profile': profile  
+    })
