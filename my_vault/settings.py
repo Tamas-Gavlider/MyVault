5ec99bde97a734208f008be0de26834193950dbc
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-10-23 11:40:14 +0000

    create sending and receiving address for user

--- my_vault/settings.py
+++ my_vault/settings.py
@@ -138,7 +138,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
