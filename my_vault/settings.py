7f158112d40aa38919b99fba13ddf5fe3e256dbb
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-10-24 12:05:44 +0000

    Add email notifications  email address/pwd to env.py

--- my_vault/settings.py
+++ my_vault/settings.py
@@ -127,8 +127,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tamasgavlider@gmail.com'
-EMAIL_HOST_PASSWORD = 'Cryptopadre89'
+EMAIL_HOST_USER = os.getenv('EMAIL_ADDRESS)
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD)
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER 
 """
 
