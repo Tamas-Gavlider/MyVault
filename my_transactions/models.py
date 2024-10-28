018f5362a2cbca1f2e33f3895c7b5e1d80b9495e
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-10-28 12:37:14 +0000

    Change database structure of Transactions model

--- my_transactions/models.py
+++ my_transactions/models.py
@@ -13,16 +13,20 @@ class Transactions(models.Model):
         ('Cancelled','Cancelled')
     ]
     
+    TRANSACTION_TYPE = [
+        ('Withdraw','Withdraw'),
+        ('Deposit','Deposit'),
+        ('Sent','Sent'),
+        ('Received','Received')
+    ]
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    withdraw = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    deposit = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    money_sent = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    money_received = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    type = models.CharField(max_length=50, choices=TRANSACTION_TYPE)
     status = models.CharField(max_length=50, choices=TRANSACTIONS_STATUS, default='Pending')
     date = models.DateTimeField(auto_now_add=True)
     
     
     
     def __str__(self):
-        return f'{self.withdraw} - {self.deposit} - {self.date}'
+        return f'{self.type} - {self.status} - {self.date}'
         
