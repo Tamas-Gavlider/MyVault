c02b415923d9a632c18badc8e204f30879f7815c
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-11-11 18:56:30 +0000

    Add transactions to view/move templ. to dashboard folder

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from my_transactions.models import Transactions
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
-    return render(request,'dashboard.html')
+    transactions = Transactions.objects.get(user=request.user)
+    return render(request,'dashboard.html', {'transactions':transactions})
