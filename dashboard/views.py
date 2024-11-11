7f7af7e5c41ed7b65afae68693c73e7e22eb81a0
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-11-11 22:54:43 +0000

    Add profile to dashboard views/chart.js to dashb. html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from my_transactions.models import Transactions
+from my_profile.models import Profile
 
 # Create your views here.
 
 @login_required
 def dashboard(request):
-    transactions = Transactions.objects.get(user=request.user)
-    return render(request,'dashboard.html', {'transactions':transactions})
+    transactions = Transactions.objects.filter(user=request.user)
+    profile = Profile.objects.get(user=request.user)
+    balance = profile.balance
+    return render(request,'dashboard.html', {'transactions' : transactions, 'profile': profile})
