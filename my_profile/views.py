4f2f256603cdf3f76f85e4aa1f093097550e1784
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-11-13 22:52:02 +0000

    Del UserLocation model

--- my_profile/views.py
+++ my_profile/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import logout
 from django.http import JsonResponse
 from django.core.mail import send_mail
 from django.utils import timezone
-from .models import Profile, UserLocation
+from .models import Profile
 from .forms import ProfileUpdateForm, UserUpdateForm
 import secrets
 from django.conf import settings
