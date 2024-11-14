42315146bba804dba8c345110f4aed24516b9601
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-11-14 16:41:55 +0000

    Fix flex on trans.btns/admin warning for transaction over 25k

--- my_transactions/admin.py
+++ my_transactions/admin.py
@@ -1,14 +1,27 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Transactions
 
 # Register your models here.
 
 class TransactionsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'type', 'status', 'date', 'sending_address', 'receiving_address', 'amount')
+    list_display = ('user', 'type', 'status', 'date','amount', 'sending_address', 'receiving_address')
     search_fields = ('user__username', 'sending_address', 'receiving_address')
     
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         return queryset.all()
+    
+    def high_value_transaction(self, request, extra_context=None):
+        """
+        Check transactions over 25k and warn the admin
+        """
+        high_value_transactions = self.get_queryset(request).filter(amount__gt=25000.00)
+        if high_value_transactions.exists():
+            messages.warning(
+                request,
+                f"There are {high_value_transactions.count()} transactions over 25,000."
+            )
+        return super().changelist_view(request, extra_context=extra_context)
+            
 
 admin.site.register(Transactions, TransactionsAdmin)
