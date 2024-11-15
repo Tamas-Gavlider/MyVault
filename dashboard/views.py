c1d0c61ebd8b75fd2cd65ab43f147d6884a41ea7
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-11-15 13:49:18 +0000

    design the py files in dashboard app  per pep8

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,25 +4,29 @@ from my_transactions.models import Transactions
 from django.utils.timezone import make_aware
 from my_profile.models import Profile
 from datetime import datetime, timedelta
-import pygal 
+import pygal
 
 # Create your views here.
+
 
 @login_required
 def dashboard(request):
     """
     Visualize the user monthly inflow/outflow on pie and bar charts.
-    It will reflect the sum for the current month. 
+    It will reflect the sum for the current month.
     """
     today = datetime.today()
-    
+
     # Get the start and end of the current month
     start_of_month = today.replace(day=1)
-    end_of_month = today.replace(month=today.month % 12 + 1, day=1) - timedelta(days=1)
-    
+    end_of_month = today.replace(month=today.month % 12 + 1,
+                                 day=1) - timedelta(days=1)
+
     # Filter transactions for the current month
-    transactions = Transactions.objects.filter(user=request.user, date__range=(make_aware(start_of_month), make_aware(end_of_month)))
-    
+    transactions = Transactions.objects.filter(
+        user=request.user, date__range=(make_aware(
+         start_of_month), make_aware(end_of_month)))
+
     profile = Profile.objects.get(user=request.user)
     balance = profile.balance
     totalWithdraw = 0
@@ -43,7 +47,6 @@ def dashboard(request):
     bar_chart.add('Deposit', totalDeposit)
     bar_chart.add('Sent', totalSent)
     bar_chart.add('Received', totalReceived)
-    
     chart_svg = bar_chart.render().decode('utf-8')
     bar_chart = pygal.HorizontalBar()
     current_month = datetime.now().strftime("%B")
@@ -55,8 +58,8 @@ def dashboard(request):
     chart_horizontal = bar_chart.render().decode('utf-8')
 
     return render(request, 'dashboard.html', {
-        'transactions': transactions, 
-        'profile': profile, 
-        'chart_svg': chart_svg, 
+        'transactions': transactions,
+        'profile': profile,
+        'chart_svg': chart_svg,
         'chart_horizontal': chart_horizontal,
     })
