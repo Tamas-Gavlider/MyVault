82310fc4e773d952a87dac6b4a1fed319357c4f0
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-11-18 10:09:14 +0000

    Add test for sending/receiving addresses

--- my_profile/tests.py
+++ my_profile/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from my_profile.models import Profile, DeletedProfileLog
 from django.utils import timezone
+from .views import generate_unique_sending_address, generate_unique_receiving_address
 import sys
 
 # Create your tests here.
@@ -39,14 +40,29 @@ class DeletedProfileLogTests(TestCase):
         self.profile = Profile.objects.create(user=self.user)
 
     def test_deleted_profile_log_entry(self):
-        # Ensure the log entry is created
         DeletedProfileLog.objects.create(
             username=self.user.username,
             email='testuser@example.com',
             deletion_date='2024-11-18'
         )
 
-        # Retrieve and verify the log entry
         log_entry = DeletedProfileLog.objects.get(username='testuser')
         self.assertEqual(log_entry.username, 'testuser')
         self.assertEqual(log_entry.email, 'testuser@example.com')
+        
+class UniqueAddressTests(TestCase):
+    def test_generate_unique_sending_address(self):
+        """
+        Ensure the function generates a unique sending address by testing the length of it.
+        """
+        address = generate_unique_sending_address()
+        self.assertEqual(len(address), 20) 
+        self.assertFalse(Profile.objects.filter(sending_address=address).exists()) 
+
+    def test_generate_unique_receiving_address(self):
+        """
+        Ensure the function generates a unique sending address by testing the length of it.
+        """
+        address = generate_unique_receiving_address()
+        self.assertEqual(len(address), 20)  
+        self.assertFalse(Profile.objects.filter(receiving_address=address).exists()) 
